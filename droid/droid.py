725dcf160d2fc208d4d3d2e4eb65096f271b6c88
sincere1 <sincere412@gmail.com>
2020-03-05 22:45:07 +0200

    Handle error from AWS when stopping a pending instance or starting a stopping one

--- droid/droid.py
+++ droid/droid.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='droid_maker')
@@ -132,7 +133,11 @@ def stop_instaces(project):
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not stop {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
@@ -146,7 +151,11 @@ def start_instaces(project):
 
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not start {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
